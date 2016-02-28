2a2cef34c56052e8ec1ca55809e76c2777a8996b
Gurvir Cheema <gurvircheema@hotmail.com>
2016-02-28 14:44:35 -0800

    Add and configure acts as votable

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
 	def index
 		@pins = Pin.all.order("created_at DESC")
 	end
@@ -34,6 +34,10 @@ class PinsController < ApplicationController
     redirect_to root_path, notice:"Pin deleted"
 	end
   
+	def upvote
+		@pin.upvote_by current_user
+		redirect_to :back
+	end
 	private
 		def pin_params
 			params.require(:pin).permit(:title, :description, :image)
